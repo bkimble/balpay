4c31016133d616f13f45ef20336737c8ac347e8d
Billy Kimble <basslines@gmail.com>
2014-06-17 22:29:32 -0700

    got card API calls working, now need to deal with async calls not returning in time

--- test/balpay_test.js
+++ test/balpay_test.js
@@ -1,14 +1,25 @@
 require('./test_helper.js');
-var balpay = require('../lib/balpay.js');
+var Balpay = require('../lib/balpay.js');
 var scenario = require('gerbil').scenario;
 
 var good_test_cc_num = '4444444444444444';
-var good_test_cc_exp = '12/15';
+var good_test_cc_exp_month = '12';
+var good_test_cc_exp_year= '15';
+var good_test_cc_cvv = '123';
 
-var Nipple = require('nipple');
-
-scenario("Charging a card", {
+scenario("Creating a card", {
   "should succeed if the card number, expiration, and cvv are all valid": function(g){
-	g.assertEqual(true, balpay.charge(good_test_cc_num, good_test_cc_exp));
+    balpay = new Balpay();
+    var response = balpay.card(good_test_cc_num, good_test_cc_exp_month, good_test_cc_exp_year, good_test_cc_cvv);
+    // this is an async call. need to use promises here to capture return value
+    g.assertEqual(true, response);
   }
 });
+
+// scenario("Charging a card", {
+//   "should succeed if the card number, expiration, and cvv are all valid": function(g){
+//     balpay = new Balpay('44444444444444444','12','15','123');
+//     g.assertEqual(true, balpay.charge("34","test", "test description"));
+//   }
+// });
+// 
