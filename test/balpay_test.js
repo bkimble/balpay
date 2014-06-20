7a7d1879f8bed96f3ce830ab91486b084f286863
Billy Kimble <basslines@gmail.com>
2014-06-20 12:36:49 -0700

    making progress on geribil+q+exceptions

--- test/balpay_test.js
+++ test/balpay_test.js
@@ -13,26 +13,19 @@ var good_test_cc_cvv = '123';
 scenario("Creating a card", {
   "should succeed if the card number, expiration, and cvv are all valid": function(g){
     balpay = new Balpay();    
+
     balpay.card(good_test_cc_num, good_test_cc_exp_month, good_test_cc_exp_year, good_test_cc_cvv).then(function(card) {
-      
-      try {
       // this is an async call. need to use promises here to capture return value
       g.assertEqual('/cards/CC4RXQ6eZ6E4lkboU8MJbPbk', card.href);
-      g.assertEqual('fuck', card.link);
-      console.log(3); 
-      console.log(1);
       g.assertEqual('CC4RXQ6eZ6E4lkboU8MJbPbk', card.id);
-      console.log(2);
-      g.assertEqual(false, true);
-      console.log(4);      
-      console.log(card);      
-      } catch(e) {
-        console.log("shit");        
-        raise(e);
+      g.assertEqual({}, card.links); 
+      g.assertEqual({a:1}, card.links); // trying to get gerbil to fail
+      g.assertEqual(false, true); // trying to get gerbil to fail      
+      console.log("werwer");
       }
-      }
-
-    );
+    ).catch(function(error) {
+      raise(error);
+    });
   }
 });
 
