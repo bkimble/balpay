591be8994895b032d50613d883436e7064d18660
Billy Kimble <basslines@gmail.com>
2014-06-22 12:40:38 -0700

    more test cases

--- myclass_test.js
+++ myclass_test.js
@@ -9,6 +9,28 @@ var Myclass = function() {
 
 }
 
+scenario("I am a monkey", {
+  "should not be true if I am a tube steak": function(g){
+    g.assertEqual('tubesteak', 'monkey');      
+  }
+});
+
+// Prints to console:
+// == Running I am a monkey ==
+//    x should not be true if I am a tube steak - assertion number 2 failed - Error: Not equal tubesteak != monkey
+//     at Function.GerbilError [as Error] (projects/balpay/node_modules/gerbil/lib/gerbil.js:164:15)
+//     at GerbilTest.Gerbil.Test.assertEqual (projects/balpay/node_modules/gerbil/lib/gerbil.js:313:24)
+//     at Object.scenario.should not be true if I am a tube steak (projects/balpay/myclass_test.js:28:7)
+//     at Gerbil.execute (projects/balpay/node_modules/gerbil/lib/gerbil.js:22:15)
+//     at Gerbil.run (projects/balpay/node_modules/gerbil/lib/gerbil.js:99:16)
+//     at scenario (projects/balpay/node_modules/gerbil/lib/gerbil.js:372:41)
+//     at Object.<anonymous> (projects/balpay/myclass_test.js:26:1)
+//     at Module._compile (module.js:456:26)
+//     at Object.Module._extensions..js (module.js:474:10)
+//     at Module.load (module.js:356:32)
+// All tests completed for I am a monkey: 0 passed, 1 failed, 0 pending, of 1 tests (1 assertions) in 0.0009999275207519531 s
+
+
 scenario("I am a potato", {
   "should not be true if I am a carrot": function(g){
     myclass = new Myclass
@@ -23,8 +45,10 @@ scenario("I am a potato", {
   }
 });
 
-scenario("I am a monkey", {
-  "should not be true if I am a tube steak": function(g){
-    g.assertEqual('tubesteak', 'monkey');      
-  }
-});
+// Does not print to console like the monkey scenario does:
+// == Running I am a potato ==
+//    * should not be true if I am a carrot (1 assertions)
+// All tests completed for I am a potato: 1 passed, 0 failed, 0 pending, of 1 tests (1 assertions) in 0.0009999275207519531 s
+
+
+//In A  Gerbil scenario, When I try to assert a failing case whilst inside of a Q Promise .then() block, it appears Gerbil gobbles up my exceptions. How can I fix this so that the assertions behave the same way inside or outside of a .then() ? Pastebin up at http://pastebin.com/Xp2UwmMP
--- test/balpay_test.js
+++ test/balpay_test.js
@@ -1,11 +1,13 @@
 require('./test_helper.js');
 var Balpay = require('../lib/balpay.js');
+var Card = require('../lib/card.js');
 var scenario = require('gerbil').scenario;
 var Replay = require("replay");
+var smoking = require('smoking');
 //Replay.mode = "record";
 Replay.mode = "record";
 
-var good_test_cc_num = '4444444444444444';
+var good_test_cc_num = '4111111111111111';
 var good_test_cc_exp_month = '12';
 var good_test_cc_exp_year= '15';
 var good_test_cc_cvv = '123';
@@ -22,23 +24,33 @@ scenario("Creating a card", {
       g.assertEqual({a:1}, card.links); // trying to get gerbil to fail
       g.assertEqual(false, true); // trying to get gerbil to fail      
       console.log("werwer");
-      }
-    ).catch(function(error) {
-      raise(error);
-    });
-  }
+    }
+  ).catch(function(error) {
+    raise(error);
+  });
+}
 });
 
 scenario("Charging a card", {
+  "should fail if the card doesnt have access to be debited": function(g){
+    balpay = new Balpay();
+
+    var card = smoking(Card, { id: 'CC55eeXXeeXXeeXXeeXXeeXX' });
+    
+    balpay.charge(card, "1.00", "test", "test description").then(function(output) {
+      console.log(output);
+    });
+  },
+    
     "should succeed if the card number, expiration, and cvv are all valid": function(g){
       balpay = new Balpay();    
       balpay.card(good_test_cc_num, good_test_cc_exp_month, good_test_cc_exp_year, good_test_cc_cvv).then(function(card) {        
         balpay.charge(card, "1.00", "test", "test description").then(function(output) {
           console.log(output);
         });
-        }
-      ).catch(function(error) {
-        raise(error);
-      });
-    }
-  });
+      }
+    ).catch(function(error) {
+      raise(error);
+    });
+  }
+});
